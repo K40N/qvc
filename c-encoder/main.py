ec7e6fb05463b501a346cc2a9935a180a7fab440
spin-ice <tacos12368@tutanota.com>
2023-10-24 14:35:54 -0500

    some checks

--- c-encoder/main.py
+++ c-encoder/main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 CHUNK_TIME = 16 # Frames per chunk
 WIDTH = 32
@@ -11,7 +12,12 @@ def get_chunk_array(chunk_i: int):
     for i in range(start, start + CHUNK_TIME):
         rgb = cv2.imread(FRAMES_PATH(i))
         img = rgb[:,:,0] > (255//2)
-        frames.append(img)
-    return frames
+        frames.append(img.reshape(WIDTH, HEIGHT, 1))
+    result = np.concatenate(frames, axis=2)
+    t = CHUNK_TIME // 3
+    for x in range(WIDTH // 2):
+        for y in range(HEIGHT // 2):
+            assert result[x,y,t] == frames[t][x,y]
+    return result
 
 print(get_chunk_array(6))
