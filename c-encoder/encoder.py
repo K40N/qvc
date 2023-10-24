3b7d4e7e6f9b793c2288378b2b4d27d4d078d015
spin-ice <tacos12368@tutanota.com>
2023-10-24 15:32:45 -0500

    small changes

--- c-encoder/encoder.py
+++ c-encoder/encoder.py
@@ -3,9 +3,9 @@ import numpy as np
 from box import Box
 
 class ChunkEncoder:
-    def __init__(self, xyt_volume: np.array):
-        self.xyt_volume = xyt_volume
-        ...
+    def __init__(self, xyt_volume_orig: np.array):
+        # Here, (True, False) => (ok to place, not)
+        self.xyt_volume = xyt_volume_orig.copy()
 
     def emit_box(self) -> Box:
         return ...
