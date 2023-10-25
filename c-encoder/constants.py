6bb53f3595d39374242096324bb8b69de148f2e4
spin-ice <tacos12368@tutanota.com>
2023-10-25 00:24:23 -0500

    good version, will take 14 tho

--- c-encoder/constants.py
+++ c-encoder/constants.py
@@ -1,7 +1,7 @@
-CHUNK_TIME = 4 # Frames per chunk
+CHUNK_TIME = 16 # Frames per chunk
 WIDTH = 32
 HEIGHT = 32
-BOXES_PER_CHUNK = 16
+BOXES_PER_CHUNK = 64
 LEN_SECONDS = 32
 
 VIDEO_NAME = "bad-apple"
