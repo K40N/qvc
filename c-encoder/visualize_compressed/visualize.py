f6b31354534b0057d873de89d65d98d0a9a9366d
spin-ice <tacos12368@tutanota.com>
2023-10-24 20:09:24 -0500

    thing part 2

--- c-encoder/visualize_compressed/visualize.py
+++ c-encoder/visualize_compressed/visualize.py
@@ -1,13 +1,27 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from box import Box, EncodedChunk
+
+import pickle as pkl
 
 CHUNK_TIME = 16 # Frames per chunk
 WIDTH = 32
 HEIGHT = 32
 
+VIDEO_NAME = "bad-apple"
+
 def decode_chunk_classical(chunk: EncodedChunk) -> list[np.array]:
-    frames = [ np.zeros(WIDTH, HEIGHT) for _ in CHUNK_TIME ]
+    frames = [ np.zeros(WIDTH, HEIGHT, dtype=np.bool) for _ in CHUNK_TIME ]
     for box in chunk.boxes:
-        
+        for x, y, t in box.member_coords():
+            frames[t][x, y] = True
     return frames
+
+with open(f"../encoded_{VIDEO_NAME}.pkl", "rb") as f:
+    encoded_chunks = pkl.load(f)
+
+all_frames = []
+for chunk in encode_chunks:
+    all_frames.append(decode_chunk_classical(chunk))
+print(all_frames)
